f163f8c2523a320e34ba9258f300e034d27fc936
ayuarista <aristaadewi8@gmail.com>
2025-08-07 19:27:18 +0800

    override redirect URL create category from edit to index and and add custom notifications

--- app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -2,11 +2,32 @@
 
 namespace App\Filament\Resources\CategoryResource\Pages;
 
-use App\Filament\Resources\CategoryResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\CategoryResource;
 
 class CreateCategory extends CreateRecord
 {
+    
     protected static string $resource = CategoryResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return null;
+    }
+
+    protected function AfterCreate(): void
+    {
+        Notification::make()
+            ->success()
+            ->title('Category created successfully')
+            ->body('You can now add posts to this category.')
+            ->send();
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
